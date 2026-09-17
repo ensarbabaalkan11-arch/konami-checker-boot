132cef801bae4e7a5845d86b4c8c857e08eac978
ensarbabaalkan11-arch <ensarbabaalkan11@gmail.com>
2026-09-17 16:24:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from datetime import datetime
 # ─────────────────────────────────────────────
 #  KONFİGÜRASYON
 # ─────────────────────────────────────────────
-BOT_TOKEN = os.getenv("BOT_TOKEN", "BURAYA_TOKEN_YAZ")
+BOT_TOKEN = os.getenv(8879666700:AAGfBfwt7SnvFPusfW82cnBaKu2JwhZG68Y)
 ADMIN_ID  = 7969180514
 
 HITS_FILE   = "Roblox-Hits.txt"
